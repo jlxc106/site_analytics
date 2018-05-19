eb248d902e2072fb6c9529986114d4c258a5be30
jlxc106 <jaylim106@gmail.com>
2018-05-19 07:04:56 -0700

    enable cors

--- server/server.js
+++ server/server.js
@@ -17,6 +17,27 @@ const app = express();
 app.use(express.static(publicPath));
 app.use(express.static(chartPath));
 
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.use((req, res, next) => {
     var now = new Date().toString();
     
